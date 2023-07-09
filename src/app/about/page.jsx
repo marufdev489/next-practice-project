d58b71f2aae03533458151c8975cf61061dedd78
Maruf <marufdev489@gmail.com>
2023-07-09 17:37:27 +0600

    First commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './about.module.css'
+
+const About = () => {
+  return (
+    <div className='container'>about page</div>
+  )
+}
+
+export default About
