d6804f95dc95b5c1ff0c188e5c6b938e8a43a8a3
Maruf <marufdev489@gmail.com>
2023-07-10 17:16:08 +0600

    Ui design completed

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from './page.module.css'
 
 const Blog = () => {
   return (
-    <div>blog page</div>
+    <div className={styles.mainContainer}>
+      <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={item.img}
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>{item.title}</h1>
+          <p className={styles.desc}>{item.desc}</p>
+        </div>
+      </Link>
+  </div>
   )
 }
 
